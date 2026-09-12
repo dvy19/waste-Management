f96ba3116e2ea3f92fcaeb1eb10f102eb8eea849
dvy19 <divyadixit.311204@gmail.com>
2026-09-12 14:11:18 +0530

    middleware added

--- backend/src/routes/itemRoutes.js
+++ backend/src/routes/itemRoutes.js
@@ -4,16 +4,18 @@ const express=require("express")
 const router=express.Router()
 
 const upload=require('../middleware/upload')
+const authMiddleware=require('../middleware/authMiddleware')
 
 const {createItemReq , getItemReq , getItemById , analyzeWasteImage , getUserStats}=require('../controllers/ItemController')
+const authMiddleware = require("../middleware/authMiddleware")
 
-router.post('/create-item' , createItemReq)
+router.post('/create-item' , authMiddleware , createItemReq)
 
-router.get('/get-item' , getItemReq)
+router.get('/get-item' , authMiddleware, getItemReq)
 
-router.get('/get-item-id/:trackingId' , getItemById)
+router.get('/get-item-id/:trackingId' ,authMiddleware, getItemById)
 
-router.get('/get-user-stats' , getUserStats)
+router.get('/get-user-stats' , authMiddleware,getUserStats)
 
 router.post(
     "/analyze-waste",
